684b41dcdd9a066f372218056cb4f0fdf89b1ab8
Claire Harmer <claire.f.harmer@gmail.com>
2018-06-19 23:53:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,32 +18,28 @@ end
 
 def position_taken?(board, index) 
   if board[index] == " " || board[index] == "" || board[index] == nil 
-    return false
-    elsif board[index] == "X" || board[index] == "O"
+      return false
+  elsif board[index] == "X" || board[index] == "O"
       return true
   end
 end
 
 def valid_move?(board, index)
-    if (index.between?(0,8)) && !(position_taken?(board, index))
+  if (index.between?(0,8)) && !(position_taken?(board, index))
       return true
-   else
+  else
       return false 
-end 
+  end 
 end
 
- 
- 
 def turn(board)
   puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(index, board) 
     return move(board, input, index = "X")
   else 
     turn(board)
-  end
-  display_board(board)
   end
 end
  
