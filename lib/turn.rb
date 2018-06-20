3d4dfcfb43f497c2bf662b7c2401fb2a21026842
Claire Harmer <claire.f.harmer@gmail.com>
2018-06-20 00:36:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,7 @@ def display_board(board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 
-def input_to_index(input = [ "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def input_to_index(input)
     input.to_i - 1
 end
 
